14ea1d4ed8f2c64677a2a609ccefa5a0643a39a1
ariel <arzucker@g.jct.ac.il>
2022-03-08 14:22:51 +0200

    Tests

---
+++ unittest/primitives/PointTest.java
@@ -0,0 +1,24 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void distanceSquared() {
+    }
+
+    @Test
+    void distance() {
+    }
+}
---
+++ unittest/primitives/VectorTest.java
@@ -0,0 +1,25 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+
+}
