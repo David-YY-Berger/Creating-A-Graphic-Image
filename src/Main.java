a7e02e77e021253adab095b192988cee711e3477
davidberger2448@gmail.com <davidberger2448@gmail.com>
2022-03-10 13:23:03 +0200

    h

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,6 @@ import static primitives.Util.*;
  */
 public final class Main {
 
-    /**
-     * Main program to test initial functionality of the 1st stage
-     *
-     * @param args irrelevant here
-     */
     public static void main(String[] args) {
 
         out.println("If there were no any other outputs - all tests succeeded!");
