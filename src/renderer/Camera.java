9f9bee71d14c4079a1158dbd51108dae97f6248f
davidberger2448@gmail.com <davidberger2448@gmail.com>
2022-03-29 15:50:17 +0300

    made empty funcs for targil4 db

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -1,26 +1,54 @@
-//package renderer;
-//import primitives.*;
-//
-//
-///**
-// * Class holds Camera; we trace rays from the camera to each object
-// */
-//public class Camera {
-//
-//    private Point p0;
-//    private Vector v_to;
-//    private Vector v_up;
-//    private Vector v_right;
-//
-//    private double height;
-//    private double width;
-//    private double length;
-//
-//    public Camera(Point _p0, Vector _v_to, Vector _v_up) {
-//
-//        //checks that Vectors are orthogonal
-//
-//    }
-//
-//
-//}
+package renderer;
+import primitives.*;
+
+
+/**
+ * Class holds Camera; we trace rays from the camera to each object
+ */
+public class Camera {
+
+    private Point p0;
+    private Vector v_to;
+    private Vector v_up;
+    private Vector v_right;
+
+    private double height;
+    private double width;
+    private double length;
+
+    private double distanceFromVP;
+
+    public Camera(Point _p0, Vector _v_to, Vector _v_up) {
+
+        //checks that Vectors are orthogonal (otherwise, throws ex),
+
+//code...
+
+        // creates v_right, saves all as normal vectors
+
+    }
+
+    public Camera setVPSize(double width, double height){
+
+        //code...
+
+        Camera res = new Camera(new Point(0, 0, 0), new Vector(0, 0, 0), new Vector(0, 0, 0));
+
+        return res;
+    }
+    public Camera setVPDistance(double distance){
+        //code...
+
+        return this; //?
+    }
+
+    public Ray constructRay(int nX, int nY, int j, int i){
+
+        //code...
+
+
+        Ray res = new Ray(new Point(0, 0, 0), new Vector(0, 0, 0));
+        return res;
+    }
+
+}
