4ce1b5954dcbe83a2a2189fd471d3d5693520f3e
davidberger2448@gmail.com <davidberger2448@gmail.com>
2022-03-30 13:06:24 +0300

    db

---
+++ unittests/integrationTests/CameraAndRayTracingTests.java
@@ -0,0 +1,19 @@
+package integrationTests;
+
+/**
+ * Integration tests for interaction between the Camera and finding intersections of rays and geometries
+ */
+public class CameraAndRayTracingTests {
+
+
+    public void testWithSphere() {
+
+    }
+    public void testWithPlane() {
+
+    }
+    public void testWithTriangle() {
+
+    }
+
+}
