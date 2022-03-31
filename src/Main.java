8cdf34cee8fbe132f8a656e7fd70d5e94015a86b
davidberger2448@gmail.com <davidberger2448@gmail.com>
2022-03-31 08:07:57 +0300

    advanced in integration test methods db

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public final class Main {
 
     public static void main(String[] args) {
 
-      Vector v1 = new Vector(-3, 4,5);
-      System.out.println(v1.normalize());
+
+      System.out.println("no bugs BH!");
 
 
 
--- unittests/integrationTests/CameraAndRayTracingTests.java
+++ unittests/integrationTests/CameraAndRayTracingTests.java
@@ -1,17 +1,37 @@
 package integrationTests;
+import org.junit.jupiter.api.Test;
+import primitives.Ray;
 
 /**
  * Integration tests for interaction between the Camera and finding intersections of rays and geometries
  */
 public class CameraAndRayTracingTests {
 
-
+    @Test
     public void testWithSphere() {
 
+    
+    
+    
+    
+    
+    
     }
+
+    /**
+     * Integration test method for {@link renderer.Camera#constructRay(int, int, int, int)} 
+     * and {@link geometries.Plane#findIntersections(Ray)}
+     */
+    @Test
     public void testWithPlane() {
 
+        
+        
+        
+        
+        
     }
+    @Test
     public void testWithTriangle() {
 
     }
