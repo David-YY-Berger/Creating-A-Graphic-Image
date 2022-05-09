0e59b8330667306522473cfebe664ca2e3b11ae4
davidberger2448@gmail.com <davidberger2448@gmail.com>
2022-05-09 17:51:42 +0300

    began targil 7 db

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,6 @@ public final class Main {
     public static void main(String[] args) {
 
 
-
-//        Scene myScene = new Scene.Builder("davidsTestScene")
-//                .setAmbientLight(new Color(255, 255, 0), new Double3(.1, .1, .1))
-//                .setBackground(new Color(100, 100, 100))
-//                .addGeometry(new Sphere(2, new Point(6, 0, 0)))
-//                .addGeometry(new Sphere(2, new Point(6, 3, 0)))
-//                .build();
-//
-//        Camera cam = new Camera(Point.ZERO, new Vector(1, 0, 0), new Vector(0, 1, 0))
-//                .setVPDistance(1).
-//                setVPSize(100, 100)
-//                .setImageWriter(new ImageWriter("davidsTestImage", 100, 100))
-//                .setRayTracerBase(new RayTracerBasic(myScene));
-//
-//        cam.printGrid(50, new Color(50, 50, 50));
-//        cam.writeToImage();
-
-
       System.out.println("no bugs BH!");
 
 
